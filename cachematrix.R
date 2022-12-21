04d53fec3fa2d14f54a23fe0b9ee8d807dfd0475
austinDm21 <121192487+austinDm21@users.noreply.github.com>
2022-12-21 13:52:53 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These two functions work in tandem to cache the inverse of a matrix
+## Instead of calculating the inverse a second time, if cached inverse
+## exists, the cache is read from memory
 
-## Write a short comment describing this function
+## First, define the list of functions that will be used to cache the
+## inverse of the matrix, namely:
+
+## set - write the starting matrix to memory
+## get - read the starting matrix
+## setinv - calculate and cache the matrix inverse
+## getinv - read the cached inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    i <- matrix(NaN)
+    set <- function(y) {
+        x <<- y
+        i <<- matrix(NaN)
+    }
+    get <- function() x
+    setinv <- function(solve) i <<- solve
+    getinv <- function() i
+    list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## Second, call the functions from makeCacheMatrix to either calculate the
+## inverse of the matrix or read the cache, as needed
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    i <- x$getinv()
+    if(!all(is.na(i))) { ## check if cached matrix is null; if not, read cache
+        message("retrieving cached inverse")
+        return(i)
+    }
+    data <- x$get() ## read starting matrix 'x'
+    i <- solve(data,...)
+    x$setinv(i) ## calculate the inverse of 'x'
+    i ## Return a matrix that is the inverse of 'x'
 }
